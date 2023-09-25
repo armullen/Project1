1f0e63be5ec3931965e0e747ce811cf1a3474d03
Amber Mullen <121705867+armullen@users.noreply.github.com>
2023-09-25 14:13:50 -0500

    reworking iterations

--- src/views/girls/Bachelorettes.vue
+++ src/views/girls/Bachelorettes.vue
@@ -1,5 +1,38 @@
 <!-- all the female dogs will be pulled from the main database by the gender field and put there.  There will be a slector box to add these to your own list of selected dogs stored by email -->
 
 <template>
-    <h1>Lucky Ladies</h1>
+    <h1>Lovely Ladies</h1>
+    <ul>
+        <li v-for='dog in dogs' :key="dog.id">
+            {{ dog.name }}
+        </li>
+    </ul>
 </template>
+
+
+<script lang="ts">
+import { ref } from 'vue'
+
+
+interface Dog {
+name: string;
+age: number;
+breed: string;
+gender: string;
+id: number;
+}
+    
+export default{
+        name: 'DogList',
+        setup() {
+            const dogs = ref<Dog[]>([
+                {name: "Matilda", age: 3, breed: "Great Dane", gender: "Female", id: 4},
+                {name: "Claire", age: 7, breed: "Bichon Frise", gender: "Female", id: 5},
+                {name: "Phoebe", age: 1, breed: "Rottweiler", gender: "Female", id: 6},
+            ])
+            return {
+                dogs
+            }
+        },
+    }
+</script>

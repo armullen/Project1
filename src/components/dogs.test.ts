c1f1eeb7a8e73bdbfe66ccde30f6acc68bf3ac5b
Amber Mullen <121705867+armullen@users.noreply.github.com>
2023-09-27 14:07:00 -0500

    still writing test code

--- src/components/dogs.test.ts
+++ src/components/dogs.test.ts
@@ -1,30 +1,30 @@
 import { expect, test } from 'vitest'
-import { getNextId } from './dogs.vue'
-
-const dogs = [
-        {id: 1},
-        {id: 2},
-        {id: 3},
-    
-];
-
-test('gets next id', () => {
-    const result = getNextId(dogs);
-    expect(result).toBe(4);
-
-})
+import { mount } from '@vue/test-utils'
+import { Dogs } from './index.ts'
 
 
+test('creates a new dog', async () => {
+    const wrapper = mount(Dogs)
+    expect(wrapper.findAll('[data-test="Dogs"]')).toHaveLength(1)
+
+    await wrapper.get('[data-test="dog-name"]').setValue('Fido')
+    await wrapper.get('[data-test="dog-breed"]').setValue('Poodle')
+    await wrapper.get('[data-test="dog-age"]').setValue('3')
+    await wrapper.get('[data-test="dog-gender"]').setValue('Male')
+    await wrapper.get('[data-test="dog-form"]').trigger('submit')
+
+    expect(wrapper.findAll('[data-test="dog"]')).toHaveLength(2)
+});
 
 
-// function getNextId(): number {
-    
-//     let result = 1;
-//     for(let i = 0; i < dogs.length; i++) {
-//         if(dogs[i].id >= result) {
-//             result = dogs[i].id + 1;
-//         }
-//     }
+// describe('remove a dog button is pushed' () => {
+//     it('should remove the dog from the Dog array', async() => {
+//     })
+//         const wrapper = mount(Dogs)
+//         await wrapper.find('button').trigger('click')
+// })
 
-//     return result;
+
+
+
     

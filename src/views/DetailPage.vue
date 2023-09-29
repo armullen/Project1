72aab91ed0fa22a63cff7ff0edd23a70d410924e
Amber Mullen <121705867+armullen@users.noreply.github.com>
2023-09-29 16:18:47 -0500

    still stuck on routing, but building detail view

--- src/views/DetailPage.vue
+++ src/views/DetailPage.vue
@@ -1,27 +1,27 @@
 <!-- a page for each individual dog to show information when hyperlink name is clicked on all dogs or girls and boys page, will also have a contact button and favorites button that will save as a favorite in a seperate array -->
 
 <template>
-           <h1>
-               Dog {{ $route.params.id }} Detail Page 
-            </h1> 
-     
-    
+    <div>
+        <p>
+            Hello, my name is {{ Dog.name }} and I am a {{ Dog.breed }}. I am {{ Dog.age }} years old.
+        </p> 
+        <button>Add {{ Dog.name }} to my favorites</button>     
+        <button>Contact my humans</button>     
+    </div>
+        
 </template>
 
 <script setup lang="ts">
-   
-import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
+import  Dog  from '../components/dogs.vue';
+// import { computed } from 'vue';
+// import { useRoute } from 'vue-router';
 
-const queryDog = ref({})
-const isLoading = ref(true)
-const route = useRoute()
+//     setup() {
+//         const route = useRoute();
+//         const id = computed(() => route.params.id);
+//    return { id }
+//     }
 
-onMounted(async () => {
-    const response = await fetch(`http://localhost:5173/${route.params.id}`)
-    queryDog.value = response()
-    isLoading.value = false
-})
 
 
 </script>

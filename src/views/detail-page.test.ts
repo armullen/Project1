c70de30c108f246281d5107cc82f634ddbd961c0
Amber Mullen <121705867+armullen@users.noreply.github.com>
2023-10-10 11:42:45 -0500

    working on test code for detail page

--- src/views/detail-page.test.ts
+++ src/views/detail-page.test.ts
@@ -2,22 +2,31 @@
 //test functionality of clicking on an individual dog and getting their info
 
 import DetailPageVue from "./DetailPage.vue";
-import { expect, test } from "vitest";
+import { createTestingPinia } from '@pinia/testing';
+import { useDogsStore } from "../stores";
+import { expect, test, vitest } from "vitest";
 import { mount } from "@vue/test-utils";
+
+
+const wrapper = mount(DetailPageVue, {
+    global: {
+        plugins: [createTestingPinia({ createSpy: vitest.fn}),],
+        
+    }
+})
+
+const store = useDogsStore()
+
+store.name = 'dogStore'
 
 
 //test go back button
 test('go back button', async () => {
-        const wrapper = mount(DetailPageVue);
         
-        expect(wrapper.toFind('button')).onclick('Go Back')
+        expect(wrapper.find('button')).onclick('Go Back')
 
         await wrapper.vm.$nextTick();
-        
+
         wrapper.vm.goBack();
-
-        
-
-    }
-        )
+})
    

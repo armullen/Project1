46cc52339085e99aad578635e2a54489dc7b119e
Amber Mullen <121705867+armullen@users.noreply.github.com>
2023-10-11 14:26:42 -0500

    mid day checkin

--- src/stores/dogStore.test.ts
+++ src/stores/dogStore.test.ts
@@ -1,21 +1,25 @@
 
 import { useDogsStore } from "./dogsStore";
 import { test, expect, vitest } from "vitest";
-import { createTestingPinia } from "@pinia/testing";    
+import { createTestingPinia } from "@pinia/testing";   
+import { nextTick } from "vue";
 
 const testingPinia = createTestingPinia({ createSpy: vitest.fn })
 const store = useDogsStore(testingPinia)
 
 /////////add a dog test
 
-test('add a dog', () => {
+test('add a dog', async () => {
 
 expect(store.dogsList).toHaveLength(0);
 
 store.addDog({name: 'Fido', age: 3, breed: 'Bulldog', gender: 1})
 
+await nextTick()
 
-expect(store.dogsList).toHaveLength(1);
+expect(store.dogsList).toHaveLength(1)
 })  
+
+/////////delete a dog test
     
    
